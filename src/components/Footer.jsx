7aea0ccfe726b36790469cadaadfe11790665c95
Sebamellam <sebastianmella7@gmail.com>
2023-09-22 11:54:50 -0300

    se agrega el footer y el blog

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./footer.css"
+
+export const Footer = () => <footer className="page-footer font-small blue pt-4">
+   <div className="container">
+  <footer className="py-3 my-4">
+    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Productos</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Reserva de hora</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Blog</a></li>
+    </ul>
+    <p className="text-center text-body-secondary">© 2023 Company, Inc. Todos los iquierdos reservados</p>
+  </footer>
+</div>
+</footer>
--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -1,7 +1,35 @@
 
 export const Blog = () => {
   return (
-  <h1>Blog</h1>
- 
+    <>
+      <h1>TODO LO QUE DEBES SABER SOBRE EL CUIDADO DE TUS MÁQUINAS</h1>
+      <p>Las afeitadoras eléctricas son uno de los instrumentos más versátiles y valiosos que puede tener un estilista, pero solo los mejores saben lo que es mantener a una máquina de afeitar funcionando perfectamente por años, sin tener que reemplazarla por averías o mal funcionamiento. El secreto es simple, y se encuentra en el cuidado que le des tu afeitadora.
+        Por muchos años, las afeitadoras eran consideradas un instrumento exclusivo de la barbería y el estilismo de hombres, pero últimamente se han vuelto indispensables para los estilos más vistos en las portadas de revistas y las pasarelas. El reinado del pixie cut, las medias cabezas rasuradas y los cortes afiladamente rectos le debe mucho a las máquinas de afeitar, gracias a su exactitud milimétrica y su relativa facilidad; pero la alta demanda de sus funciones y su muy frecuente uso puede causar estragos hasta en la mejor y más cara afeitadora si no se le presta la suficiente atención al cuidado de cada una de sus piezas. Desde su limpieza hasta el reemplazo de alguna parte defectuosa, el cuidado de la afeitadora debe ser lo más minucioso posible si quieres que tus clippers se mantengan como tu mejor inversión
+
+        Limpia tus hojillas
+
+        Tal y como sucede con las tijeras, las navajas y los cepillos, tu afeitadora debe ser rigurosamente limpiada tras cada día de trabajo, ya que minúsculos pedazos de cabello pueden quedar entre las hojillas y ser fatales para la máquina. Toda afeitadora debe traer en su paquete original un pequeño cepillo específicamente diseñado para limpiar entre sus hojillas; asegúrate de que esté completamente seco y pásalo suavemente entre los dientes de la hojilla para deshacerte de todo residuo que pueda dañar o desgastar el metal.
+
+        Si tu afeitadora no vino con este pequeño cepillo, no te preocupes; puedes utilizar un viejo cepillo de dientes o incluso un cepillo para tinte, siempre y cuando te cerciores de que esté seco y de que se mueva con facilidad por las hojillas. Cuando veas que estén limpias, puedes utilizar un secador de cabello para soplar breve y fuertemente sobre ellas y asegurarte de que queden libres de cualquier tipo de residuo.
+
+        Ajusta y Alinea
+
+        Tu máquina de afeitar está compuesta de partes importantes que se ajustan con pequeños tornillos, y no está demás asegurarte de vez en cuando de que todo esté en su lugar. Revisa si estos tornillos están flojos y, de ser así, utiliza un destornillador adecuado para ajustarlos; sin embargo, ten cuidado de no ajustarlos demasiado y dañar el material de la afeitadora.
+
+        Un paso muy importante a la hora de hacerle mantenimiento a tu máquina de afeitar es cerciorarse de que las hojillas estén perfectamente alineadas con el cabezal en línea recta, sin dientes fuera de lugar y ninguna desviación por parte de la hojilla, lo cual puede fácilmente hacerte daño a ti o a tu cliente. Si ves que las hojillas no están alineadas con el cabezal, toma el destornillador y afloja los tornillos hasta que puedas moverla y alinearla perfectamente; cuando esté en el lugar correcto, vuelve a ajustar los tornillos, esta vez alternando entre cada uno y prestando atención a que la hojilla no se mueva de lugar. Una vez te asegures de que todo está en orden, podrás utilizar tu afeitadora como de costumbre.
+
+        No te olvides del aceite
+
+        La clave más importante para mantener a tu afeitadora funcionando como nueva durante muchos años es el aceite. Este complemento es el responsable de garantizar que el mecanismo de la hojilla se mantenga libre de rastros de cabello y suciedad que puedan dañar la máquina, además de lubricar y suavizar la fluidez de la hojilla, protegiéndola del óxido y el desgaste. Debes aceitar tu máquina inmediatamente después de cada corte que realices para así evitar que se acumulen y se atasquen los residuos; esto debe cumplirse rigurosamente y es recomendable que lo conviertas en un hábito infalible.
+
+        El aceite es muy fácil de utilizar; con la máquina apagada, aplica una gota en cada esquina de la hojilla y unas cuantas en el centro. Luego, enciende la afeitadora para que el aceite circule por todo el mecanismo y remueva todo pequeño fragmento que amenace el rendimiento de tu máquina.
+
+        Usualmente, las afeitadoras vendrán con una pequeña cantidad de aceite especial para hojillas, pero no es el suficiente para un mantenimiento constante, por lo que te sugerimos que apenas compres tu máquina de afeitar, inviertas también en un buen aceite para navajas.
+
+
+        Como puedes ver, no hay excusa para abandonar a tu fiel afeitadora a los elementos y dejar que se dañe cuando es muy fácil mantenerla trabajando como si estuviese recién salida del paquete. Solo hace falta un poco de disciplina y los utensilios adecuados para que tu máquina de afeitar desafíe el paso del tiempo y te siga entregando los mejores cortes por muchos años.</p>
+    </>
+
+
   )
 }
