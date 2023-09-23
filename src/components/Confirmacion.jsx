de8e4d1db44273e60a98087abf3fc44311e2cf40
Sebamellam <sebastianmella7@gmail.com>
2023-09-23 14:40:13 -0300

    se corrigen los props y ahora funcionan

--- src/components/Confirmacion.jsx
+++ src/components/Confirmacion.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
 
-function Agenda(props) {
-  const { name, mail, telephone, date, time } = props.reserva;
-
-  return (
-    <div className='reserva'>
-      <div className="col-md-6 mx-auto">
-        <div className="p-4 mb-3 bg-body-secondary rounded-3">
-          <div className="container-fluid py-5">
-            <h2>Confirmación de Reserva</h2>
-            <p>Nombre: {name}</p>
-            <p>Correo: {mail}</p>
-            <p>Teléfono: {telephone}</p>
-            <p>Fecha: {date}</p>
-            <p>Hora: {time}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
 export const Confirmacion = ({ reserva }) => {
-  return (
-    <div className="col-md-6 mx-auto">
-      <div className="p-4 mb-3 bg-body-secondary rounded-3">
-        <div className="container-fluid py-5">
-          <h2>Confirmación de Reserva</h2>
-          <p>Nombre: {reserva.name}</p>
-          <p>Correo: {reserva.mail}</p>
-          <p>Teléfono: {reserva.telephone}</p>
-          <p>Fecha: {reserva.date}</p>
-          <p>Hora: {reserva.time}</p>
+
+    const res = reserva[reserva.length -1]
+    console.log(res)
+    return (
+        <div className="col-md-6 mx-auto">
+            <div className="p-4 mb-3 bg-body-secondary rounded-3">
+                {
+                    res && (
+                        <div className="container-fluid py-5">
+                            <h2>Confirmación de Reserva</h2>
+                            <p>Nombre: {res.name}</p>
+                            <p>Correo: {res.mail}</p>
+                            <p>Teléfono: {res.telephone}</p>
+                            <p>Fecha: {res.date}</p>
+                            <p>Hora: {res.time}</p>
+                        </div>
+                    )
+                }
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
